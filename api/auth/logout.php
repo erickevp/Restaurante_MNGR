99cb85d33d0d44bcfe2dfb3d63c9d19b0ac2f691
erickevp <153458044+erickevp@users.noreply.github.com>
2026-03-24 17:06:17 -0700

    Add files via upload

---
+++ api/auth/logout.php
@@ -0,0 +1,13 @@
+<?php
+// Sistema de Gestión para Restaurante
+// Creador: Erick Villegas - erickevp@gmail.com
+// Fecha: 24/03/2026
+// Versión: 1.0.0
+// Licencia: GPL v3
+
+session_start();
+session_unset();
+session_destroy();
+header("Location: ../../index.php");
+exit();
+?>
